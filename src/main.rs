f91d3629e25c2ffbaf8f01817bbbf031380e512c
Jordan Conway <jordan_conway@outlook.com>
2025-09-28 22:41:54 +0100

    Add PixelMapping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::collections;
+use image::{Rgb};
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    return;
+}
+
+pub struct PixelMapping {
+    mapping: collections::HashMap<image::Rgb<u8>, Rgb<u8>>
+}
+
+pub trait TransformPixel{
+    fn transform_pixel(&self, pixel: image::Rgb<u8>) -> Option<&image::Rgb<u8>>;
+}
+
+impl TransformPixel for PixelMapping {
+    fn transform_pixel(&self, pixel: image::Rgb<u8>) -> Option<&image::Rgb<u8>>{  
+        return self.mapping.get(&pixel);
+    }
 }
